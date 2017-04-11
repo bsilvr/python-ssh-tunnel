e58803ed709c9ca6ff07e83fcd8b7b340376c7be
Bruno Silva <brunomiguelsilva@ua.pt>
2017-04-11 16:48:21 +0100

    additional files for v0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
     from distutils.core import setup, find_packages
 
 setup(
-    name='sshtunnel',
+    name='ssh-tunnel',
     version=ssh_tunnel.__version__,
     packages=find_packages(),
     license='LICENSE',
@@ -14,6 +14,5 @@ setup(
     description='Library to create ssh remote tunnels',
     keywords=['ssh', 'remote', 'tunnel'],
     url='https://github.com/bsilvr/python-ssh-tunnel',
-    # include_package_data=True,
     download_url='https://github.com/bsilvr/python-ssh-tunnel/tarball/' + ssh_tunnel.__version__,
 )
